41d12fbbce69955af337814d0f39692430e4a1ea
Samuel Reed <samuel.trace.reed@gmail.com>
2016-03-08 17:02:03 +0800

    Update babel version to 6

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import {readFile} from './parse'
 import type {Type, Property} from './types'
 import * as validate from './validate'
 
-export default {readFile, validate}
+export {readFile, validate}
 export type {Type, Property};
